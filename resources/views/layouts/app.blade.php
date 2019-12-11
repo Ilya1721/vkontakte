43ee8a26b9bf6f5fa5ea2f4be674b78c68c3e5cc
unknown <ilyavusnya@gmail.com>
2019-12-11 20:32:59 +0200

    Added footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>{{ config('app.name') }}</title>
+  <title>Добро пожаловать</title>
   <!-- Fonts -->
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -95,6 +95,10 @@
         @yield('content')
     </main>
 
+    <footer id="footer" class="text-center w-100 py-5">
+      <hr />
+      © 2019 Copyright:
+    </footer>
   </div>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 @section('content')
 <div class="content">
-  <div class="title">
-    <img id="left-phone" src="/img/android_phone.png" />
-    <img src="/img/iphone_phone.png" />
-  </div>
+  <h3>ВКонтакте для мобильных устройств</h3>
+  <p>
+     Установите официальное мобильное приложение
+     ВКонтакте<p></p> и оставайтесь в курсе новостей
+     Ваших друзей, где бы Вы ни находились.
+  </p>
+  <img id="left-phone" src="/img/android_phone.png" />
+  <img src="/img/iphone_phone.png" />
 </div>
 @endsection
