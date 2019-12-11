5d428c8a9aa3e7a24153804099b9b9036e67dc4d
unknown <ilyavusnya@gmail.com>
2019-12-11 21:39:14 +0200

    Added links to phone apps

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,5 +9,25 @@
   </p>
   <img id="left-phone" src="/img/android_phone.png" />
   <img src="/img/iphone_phone.png" />
+  <p>
+    <div class="row justify-content-center">
+      <div id="phone-link">
+        <a href="https://play.google.com/store/apps/details?id=com.vkontakte.android"
+           target="_blank"
+           class="nav-link">
+          <img src="/img/android_logo.svg" />
+          VK для Android
+        </a>
+      </div>
+      <div id="phone-link">
+        <a href="https://apps.apple.com/ru/app/id564177498"
+           target="_blank"
+           class="nav-link">
+          <img src="/img/iphone_logo.svg" />
+          VK для iPhone
+        </a>
+      </div>
+    </div>
+  </p>
 </div>
 @endsection
