f069390c727941aeec689ede751a3171ab7459f8
unknown <ilyavusnya@gmail.com>
2019-12-13 19:56:33 +0200

    Added friend list to friend page with photo link and status

--- resources/views/friends.blade.php
+++ resources/views/friends.blade.php
@@ -5,7 +5,7 @@
     <div class="col-2">
       @yield('left-navbar', View::make('layouts.left_navbar'))
     </div>
-    <div class="col-7">
+    <div class="col-8">
       <div id="friend-list">
         <div id="friend-list-header"
         class="d-flex justify-content-between
@@ -21,9 +21,34 @@
           <input id="friend-search-input" name="friends" class="form-control"
            type="text" placeholder="Поиск участников" />
         </form>
+        @for($i = 0; $i < 5; $i++)
+        <div class="row my-3 mx-2">
+          @for($j = 0; $j < 3; $j++)
+            <div id="friend-card" class="col">
+              <a href="#">
+                <img id="friend-img" src="https://sun6-19.userapi.com/c836332/v836332139/2e2e0/RNYjGOkSsyw.jpg?ava=1" />
+              </a>
+              <div class="d-flex flex-row justify-content-between mt-2">
+                <a id="friend-name" href="#">
+                  Ирада Хайт
+                </a>
+                <a href="#" class="pr-3">
+                  <img id="suggested-friend-img"
+                   src="/img/suggested_friend.png" />
+                </a>
+              </div>
+              <div class="d-flex flex-row justify-content-start">
+                <span id="friend-status">
+                  Нет общих друзей
+                </span>
+              </div>
+            </div>
+          @endfor
+        </div>
+        @endfor
       </div>
     </div>
-    <div class="col-3">
+    <div class="col-2">
 
     </div>
   </div>
